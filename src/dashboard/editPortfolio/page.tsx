c583afe46cfeeaa4566923394eaa200ebf88f80f
galuh panca wirasa <galuhpanca166@gmail.com>
2025-11-21 10:49:38 +0700

    sedang mengerjakan untuk edit protfolio

--- src/dashboard/editPortfolio/page.tsx
+++ src/dashboard/editPortfolio/page.tsx
@@ -3,16 +3,36 @@ import { Button } from "../../components/ui/button";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import GlobalLayout from "../../layouts/globalLayout/content";
-import { aboutDescription } from "../../data/aboutSection/data";
+import { aboutDescription, mySkills } from "../../data/aboutSection/data";
 
 export default function DashboardEditPortfolio() {
   const [editAboutDescription, setEditAboutDescription] =
     useState<string>(aboutDescription);
-  const [editSkills, setEditSkills] = useState<string>("");
-  const [addSkill, setAddSkill] = useState<number>(1);
+  const [addSkill, setAddSkill] = useState<object[]>(mySkills);
+
+  function handleAddSkill(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const inputForm = event.currentTarget as HTMLFormElement;
+
+    const inputSkill = inputForm.skill.value;
+    const inputIconSkill = inputForm.iconSkill.files?.[0];
+
+    if (!inputIconSkill) return;
+
+    const fileIconUrl = URL.createObjectURL(inputIconSkill);
+
+    setAddSkill((prev) => [
+      ...prev,
+      {
+        skill: inputSkill,
+        iconSkill: fileIconUrl,
+      },
+    ]);
+  }
+  console.log(addSkill);
   return (
-    <GlobalLayout>
-      <div className="bg-slate-700 w-full pb-5 pt-10 rounded-b-md">
+    <div>
+      <div className="bg-slate-700 w-full pb-5 pt-10">
         <ul className="flex justify-evenly items-center h-full">
           <div className="h-1 bg-slate-100 w-1/2 rounded-md absolute" />
           <li className="w-8 h-8 rounded-full bg-slate-400 flex items-center justify-center text-lg font-bold z-20">
@@ -29,106 +49,104 @@ export default function DashboardEditPortfolio() {
           </li>
         </ul>
       </div>
-      <div className="my-10 flex items-center justify-between">
-        <div className="w-1/3">
-          <form onSubmit={(e) => e.preventDefault()}>
+      <GlobalLayout>
+        <div className="my-10 flex items-center justify-between">
+          <div className="w-1/3">
             <div className="flex flex-col gap-y-4 mb-4">
               <div>
                 <Label>About Description</Label>
                 <textarea
                   placeholder="A brief description of yourself"
-                  className="border border-slate-800 rounded-md p-3 h-24 w-full"
+                  className="border border-slate-800 rounded-md p-3 h-28 w-full"
                   onChange={(e) => setEditAboutDescription(e.target.value)}
                   defaultValue={editAboutDescription}
                 />
               </div>
-              <div>
-                {Array.from({ length: addSkill }).map((_: any, i: number) => (
-                  <div key={i} className="mb-4 bg-slate-100 p-5 rounded-lg">
-                    <div className="mb-3">
-                      <Label>Skills</Label>
-                      <Input
-                        type="text"
-                        onChange={(e) => setEditSkills(e.target.value)}
-                        placeholder="Your Skills"
-                      />
-                    </div>
-                    <div>
-                      <Label>Icon Skills</Label>
-                      <Input type="file" accept=".png" />
-                    </div>
-                  </div>
-                ))}
-                <Button
-                  onClick={() => setAddSkill((prev: number) => prev + 1)}
-                  className="mt-3"
-                >
-                  Add Skills
+              <form
+                onSubmit={(e) => handleAddSkill(e)}
+                className="mb-4 bg-slate-100 p-5 rounded-lg"
+              >
+                <div>
+                  <Label>Skills</Label>
+                  <Input type="text" id="skill" placeholder="Your Skills" />
+                </div>
+                <div className="my-3">
+                  <Label>Icon Skills</Label>
+                  <Input
+                    type="file"
+                    accept=".png"
+                    id="iconSkill"
+                    name="iconSkill"
+                  />
+                </div>
+                <Button variant="outline" type="submit">
+                  add
                 </Button>
+              </form>
+
+              <Button
+                variant="outline"
+                className="flex items-center justify-center"
+              >
+                <span>Next and Save</span>
+                <img
+                  src="/img/editPortfolio/next.png"
+                  alt="Next"
+                  height={200}
+                  width={200}
+                  className="size-5"
+                />
+              </Button>
+            </div>
+          </div>
+          <div className="w-3/5">
+            <div className="flex items-center justify-between flex-col gap-8">
+              <div className="lg:basis-2/5 lg:max-w-full">
+                <h1 className="text-3xl font-semibold">About</h1>
+                <div className="h-1 w-14 bg-amber-400 rounded-md mt-2" />
+                <p className="text-justify mt-3 whitespace-pre-wrap wrap-break-word">
+                  {editAboutDescription}
+                </p>
               </div>
-            </div>
-            <Button
-              variant="outline"
-              className="flex items-center justify-center"
-            >
-              <span>Next and Save</span>
-              <img
-                src="/img/editPortfolio/next.png"
-                alt="Next"
-                height={200}
-                width={200}
-                className="size-5"
-              />
-            </Button>
-          </form>
-        </div>
-        <div className="w-3/5">
-          <div className="flex items-center justify-between flex-col gap-8">
-            <div className="lg:basis-2/5 lg:max-w-full">
-              <h1 className="text-3xl font-semibold">About</h1>
-              <div className="h-1 w-14 bg-amber-400 rounded-md mt-2" />
-              <p className="text-justify mt-3 whitespace-pre-wrap wrap-break-word">
-                {editAboutDescription}
-              </p>
-            </div>
-            <div className="w-full lg:basis-1/2">
-              <h1 className="text-2xl font-semibold mb-7">My Skills</h1>
-              <ul className="flex justify-evenly font-semibold text-base sm:text-lg">
-                <li className="flex flex-col items-center justify-center">
-                  <img
-                    src="/img/research.png"
-                    alt="Research"
-                    width={200}
-                    height={200}
-                    className="size-9"
-                  />
-                  <span>Marketing Research</span>
-                </li>
-                <li className="flex flex-col items-center justify-center">
-                  <img
-                    src="/img/comunication.png"
-                    alt="Communication"
-                    width={200}
-                    height={200}
-                    className="size-9"
-                  />
-                  <span>Communication</span>
-                </li>
-                <li className="flex flex-col items-center justify-center">
-                  <img
-                    src="/img/based-onData.png"
-                    alt="Based on Data"
-                    width={200}
-                    height={200}
-                    className="size-9"
-                  />
-                  <span>Based on Data</span>
-                </li>
-              </ul>
+              <div className="w-full lg:basis-1/2">
+                <h1 className="text-2xl font-semibold mb-7">My Skills</h1>
+                <ul className="flex justify-evenly font-semibold text-base sm:text-lg">
+                  <li className="flex flex-col items-center justify-center">
+                    <img
+                      src="/img/research.png"
+                      alt="Research"
+                      width={200}
+                      height={200}
+                      className="size-9"
+                    />
+                    <span>Marketing Research</span>
+                  </li>
+                  <li className="flex flex-col items-center justify-center">
+                    <img
+                      src="/img/comunication.png"
+                      alt="Communication"
+                      width={200}
+                      height={200}
+                      className="size-9"
+                    />
+                    <span>Communication</span>
+                  </li>
+                  <li className="flex flex-col items-center justify-center">
+                    <img
+                      src="/img/based-onData.png"
+                      alt="Based on Data"
+                      width={200}
+                      height={200}
+                      className="size-9"
+                    />
+                    <span>Based on Data</span>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </GlobalLayout>
+      </GlobalLayout>
+    </div>
   );
 }
--- src/data/aboutSection/data.ts
+++ src/data/aboutSection/data.ts
@@ -1,2 +1,17 @@
 export const aboutDescription =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea omnis, quaerat eligendi et natus dicta sequi, eius maiores voluptas facilis tempore, doloribus illum ipsa ullam impedit dolorem expedita tempora.";
+
+export const mySkills = [
+  {
+    skill: "Marketing Research",
+    iconSkill: "/img/research.png",
+  },
+  {
+    skill: "Communication",
+    iconSkill: "/img/comunication.png",
+  },
+  {
+    skill: "Based on Data",
+    iconSkill: "/img/based-onData.png",
+  },
+];
